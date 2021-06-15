1ed7f450c52185f99c308551aa79daf8f293637f
Rutger Vos <99033234@mydavinci.nl>
2021-06-15 10:27:59 +0200

    working on tailwind fix

--- Yugioh_browser_game/resources/views/dashboard.blade.php
+++ Yugioh_browser_game/resources/views/dashboard.blade.php
@@ -4,11 +4,14 @@
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
-
+<body>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+            @livewire('grid')
             </div>
         </div>
     </div>
+    @livewireScripts
+</body>
 </x-app-layout>
---
+++ Yugioh_browser_game/resources/views/livewire/grid.blade.php
@@ -0,0 +1,13 @@
+<div>
+@livewireStyles
+    <p>HELLO world</p>
+   
+    <h1>{{ $count }}</h1>
+    <button wire:click="increment">+</button>
+    <button wire:click="decrement">-</button>
+    <div class="grid grid-cols-3 gap-4">
+  <div>1</div>
+  <div>9</div>
+</div>
+
+</div> 
